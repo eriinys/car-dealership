fa3a137aa338f378f66efc8a8226d5c56a99fbdb
eriinys <eriinys@duck.com>
2025-10-29 15:25:30 -0500

    .

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,10 +1,7 @@
 public class Program {
     public static void main(String[] args) {
+
         UserInterface userInterface = new UserInterface();
-        DealershipFileManager fileManager = new DealershipFileManager();
-
-        fileManager.getDealership();
         userInterface.display();
-
     }
 }
