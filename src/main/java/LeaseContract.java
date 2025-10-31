00897fabee77b92074951a3c218355d179d716d6
eriinys <eriinys@duck.com>
2025-10-30 22:17:47 -0500

    setup LeaseContract

---
+++ src/main/java/LeaseContract.java
@@ -0,0 +1,25 @@
+public class LeaseContract extends Contract{
+    private double expectedEndingValue;
+    private double leaseFee;
+
+    public LeaseContract(String date, String customerName, String email, Vehicle vehicleSold) {
+        super(date, customerName, email, vehicleSold);
+        this.expectedEndingValue = vehicleSold.getPrice() * 0.5;
+        this.leaseFee = vehicleSold.getPrice() * 0.07;
+    }
+
+    @Override
+    public double getTotalPrice() {
+        totalPrice = monthlyPay * 36;
+
+        return totalPrice;
+    }
+
+    @Override
+    public double getMonthlyPay() {
+        double loanAnnual = 0.04;
+        double monthlyRate = loanAnnual / 12;
+        monthlyPay = ((vehicleSold.getPrice() - expectedEndingValue ) + leaseFee) / 36;
+        return monthlyPay;
+    }
+}
