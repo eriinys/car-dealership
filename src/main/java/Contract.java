0adcef2ff565a2bc0fac15f498e490f6c05f6339
eriinys <eriinys@duck.com>
2025-10-31 10:41:38 -0500

    added toString

--- src/main/java/Contract.java
+++ src/main/java/Contract.java
@@ -1,3 +1,6 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public abstract class Contract {
     protected String date;
     protected String customerName;
@@ -6,8 +9,11 @@ public abstract class Contract {
     protected double totalPrice;
     protected double monthlyPay;
 
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+    LocalDate today = LocalDate.now();
+
     public Contract(String date, String customerName, String email, Vehicle vehicleSold) {
-        this.date = date;
+        this.date = today.format(formatter);
         this.customerName = customerName;
         this.email = email;
         this.vehicleSold = vehicleSold;
@@ -46,5 +52,13 @@ public abstract class Contract {
     //abstract methods
     public abstract double getTotalPrice();
     public abstract double getMonthlyPay();
+
+    public String toString(){
+        return String.format("%s|%s|%s|%d|%d|%s|%s|%s|%s|%d|%.2f|", date, customerName, email, vehicleSold.getVin(), vehicleSold.getYear(), vehicleSold.getMake(), vehicleSold.getModel(), vehicleSold.getVehicleType(),vehicleSold.getColor(),vehicleSold.getOdometer(),vehicleSold.getPrice());
+
+        //DATE|CUSTOMER_NAME|CUSTOMER_EMAIL|VIN|Y
+        //EAR|MAKE|MODEL|VEHICLE_TYPE|COLOR|ODOMETER|VEHICLE_PRI
+        //CE|[contract-specific-fields]|TOTAL_PRICE|MONTHLY_PAYMENT
+    }
 }
 
